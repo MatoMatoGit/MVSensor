7b2eafc8ab5163cf4db578a34593c58f3ca78d15
Dorus <spijker.dorus@gmail.com>
2019-03-30 21:47:30 +0100

    - proj/mvsensor_fw/main: Test code.

--- proj/mvsensor_fw/main.c
+++ proj/mvsensor_fw/main.c
@@ -23,54 +23,28 @@
 #define SOFTSERIAL_TX_PORT	PORTB
 #define SOFTSERIAL_TX_PIN	PINB0
 
-volatile uint8_t wdt = 1;
-
-void WdtCallback(void);
-void Blink(SoftTimer_t tmr);
-
 int main(void)
 {
 	//DDRB |= (1 << PINB2);
-	
-	//SoftTimerInit();
-	//SoftTimer_t timer = SoftTimerCreate(1000, Blink);
-	//SoftTimerStart(timer);
-	//WdtInit(WDT_CYCLES_512K, WdtCallback);
 	//PORTB &= ~(1 << PINB2);
-	MoistureInit();
-	RgbLedInit();
-	SensorInit();
-	NotificationInit();
 	//PORTB |= (1 << SOFTSERIAL_TX_PIN);
 	
+	/* Initialize drivers. */
+	SoftTimerInit();
+	MoistureInit();
+	RgbLedInit();
 	//softSerialInit(&DDRB, &PORTB, NULL, SOFTWARE_SERIAL_RX_DISABLED, SOFTSERIAL_TX_PIN);
 	//softSerialBegin(2400);
 	
+	/* Initialize app components. */
+	SensorInit();
+	NotificationInit();
+	
+	/* Start notification component. */
+	NotificationStart();	
+
     while (1) 
     {
-		//if(wdt == 0) {
-		//	return 0;
-		//}
-		
-		//PORTB |= (1 << PINB2);
-		//_delay_ms(100);
-		//PORTB &= ~(1 << PINB2);
-		//wdt = 0;
-		INotificationRun();
-		
-			
-		//EnterSleep();
+		/* Idle. */
     }
-}
-
-void Blink(SoftTimer_t tmr)
-{
-	PORTB ^= (1 << PINB2);
-}
-
-void WdtCallback(void)
-{
-	if(wdt == 0) {
-		wdt = 1;	
-	}
 }
